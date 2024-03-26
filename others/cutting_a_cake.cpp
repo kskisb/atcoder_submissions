2a657b60a7bb962017ece637bd3dda21fc49c938
磯部光佑 <isobekosuke@MacBook-Air.local>
2024-03-26 18:06:41 +0900

    solved Cake 2

--- others/cutting_a_cake.cpp
+++ others/cutting_a_cake.cpp
@@ -6,6 +6,25 @@ using namespace std;
 
 int main() {
     int n; cin >> n;
-    vector<long long> a(n);
-    for(int i=0; i<n; i++) cin >> a[i];
+    vector<long long> a(2*n);
+    vector<vector<long long>> dp(n+2, vector<long long>(2*n+2, 0));
+    a[0] = 0;
+    for(int i=1; i<=n; i++) cin >> a[i];
+    for(int i=n+1; i<2*n; i++) a[i] = a[i-n];
+
+    for(int i=0; i<n+1; i++) {
+        for(int j=1; j<=n; j++) {
+            if((n-i)%2 == 1) {
+                dp[j][i+j] = max(dp[j+1][i+j]+a[j], dp[j][i+j-1]+a[i+j]);
+            } else {
+                if(a[j] > a[i+j]) dp[j][i+j] = dp[j+1][i+j];
+                else dp[j][i+j] = dp[j][i+j-1];
+            }
+        }
+        dp[n+1][n+i+1] = dp[1][i+1];
+    }
+
+    long long ans = 0;
+    for(int i=1; i<=n; i++) ans = max(ans, dp[i][i+n]);
+    cout << ans << endl;
 }
