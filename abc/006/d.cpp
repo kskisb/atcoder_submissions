2c09e06aaaeba49dbc5841d2da5618ec64f3f8c8
kskisb <isbkosuke@gmail.com>
2024-04-06 13:33:09 +0900

    solved a problem

---
+++ abc/006/d.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <climits>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(int i=0; i<n; i++) cin >> a[i];
+    vector<int> lis;
+
+    for(int i=0; i<n; i++) {
+        auto itr = lower_bound(lis.begin(), lis.end(), a[i]);
+        if(itr != lis.end()) *itr = a[i];
+        else lis.push_back(a[i]);
+    }
+
+    cout << n - lis.size() << endl;
+}
